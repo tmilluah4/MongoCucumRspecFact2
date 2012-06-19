ab8ed360501f20760ba4273122172c37d56d0e0d
Todd Miller <todd.miller3@yahoo.com>
2012-06-19 13:22:41 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoCucumRspecFact2::Application.config.secret_token = 'ced1e49fe08667b8166c0e99c680c388bc6aaba0131253e9f607c171835b908a504cbb20c40887922b8ef9860eaf0cc6f112294db54175f5b15a5affd500110e'
